5abb0258850fc8a2214d55936738c3ae407423d5
Mike Causer <mcauser@gmail.com>
2018-02-17 01:30:52 +1100

    Constant for MSB

--- tm1637.py
+++ tm1637.py
@@ -9,6 +9,7 @@ TM1637_CMD2 = const(192) # 0xC0 address command
 TM1637_CMD3 = const(128) # 0x80 display control command
 TM1637_DSP_ON = const(8) # 0x08 display on
 TM1637_DELAY = const(10) # 10us delay between clk/dio pulses
+TM1637_MSB = const(128)  # msb is the decimal point or the colon depending on your display
 
 # 0-9, a-z, blank, dash, star
 _SEGMENTS = bytearray(b'\x3F\x06\x5B\x4F\x66\x6D\x7D\x07\x7F\x6F\x77\x7C\x39\x5E\x79\x71\x3D\x76\x06\x1E\x76\x38\x55\x54\x3F\x73\x67\x50\x6D\x78\x3E\x1C\x2A\x76\x6E\x5B\x00\x40\x63')
@@ -194,7 +195,7 @@ class TM1637Decimal(TM1637):
         j = 0
         for i in range(len(string)):
             if string[i] == '.' and j > 0:
-                segments[j-1] |= 0b10000000
+                segments[j-1] |= TM1637_MSB
                 continue
             segments[j] = self.encode_char(string[i])
             j += 1
