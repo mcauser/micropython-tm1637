cd1c34940966905949d4c196c36f2613b6c56aae
giddyhup <giddyhup@users.noreply.github.com>
2021-05-06 12:46:37 +0200

    Followed suggestions, addressed potential issue
    Suggestions followed. Added exception handling for cases where decimal point may be missing.

--- tm1637.py
+++ tm1637.py
@@ -199,7 +199,7 @@ class TM1637(object):
             self.write(self.encode_string(string))
         self.write([_SEGMENTS[38], _SEGMENTS[12]], 2)  # degrees C
 
-    def dectemperature(self, num):
+    def dec_temperature(self, num):
         if num < -9.9: # limit to single digit negatives
             self.write([0, 0, 0, 0])
             self.show('lo')  # low
@@ -207,22 +207,25 @@ class TM1637(object):
             self.write([0, 0, 0, 0])
             self.show('hi')  # high
         else:
-            intval = int(num/1)
+            intval = abs(int(num/1))
             if num == 0: # exact zero
                 seg1 = 0b00000000
                 seg2 = self.encode_digit(0)
                 seg3 = self.encode_digit(0)
             else:
-                if abs(intval) < 10 and num > 0: # single digit positive
+                if intval < 10 and num > 0: # single digit positive
                     seg1 = 0b00000000
-                    seg2 = self.encode_digit(abs(intval))
+                    seg2 = self.encode_digit(intval)
                 else: # two digit (can only be positive)
-                    seg1 = self.encode_digit(int(abs(intval)/10))
-                    seg2 = self.encode_digit(int(num-(int(abs(intval)/10)*10)))
+                    seg1 = self.encode_digit(int(intval/10))
+                    seg2 = self.encode_digit(int(num-(int(intval/10)*10)))
                 if num < 0: # negative
                     seg1 = 0b01000000 # '-'
-                    seg2 = self.encode_digit(abs(intval))
-                seg3 = self.encode_digit(int(str(num).split('.')[1][0])) 
+                    seg2 = self.encode_digit(intval)
+                try:
+                    seg3 = self.encode_digit(int(str(num).split('.')[1][0])) # will fail if there is no decimal point
+                except:
+                    seg3 = self.encode_digit(0)
             segments = [seg1, seg2, seg3, _SEGMENTS[38]] # segments and degree character
             segments[1] |= 0x80  # colon as decimal
             self.write(segments)
